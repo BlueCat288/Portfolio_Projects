c98c4389dfad4ab9995006be415541d6e59f60a8
BlueCat288 <xinyaozhu2015@gmail.com>
2021-05-31 15:57:01 -0500

    full project2 - Welp
    full project2 - Welp

---
+++ Proj2/Frontend/yelp-app/src/yelp-api/config.ts
@@ -0,0 +1,7 @@
+/**
+ * Remove Bearer token when in production
+ */
+// export const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3';
+export const BASE_URL = 'https://intense-mountain-38710.herokuapp.com/https://api.yelp.com/v3';
+
+export const BEARER_TOKEN = 'nZ8n1PV4CyC4oq7XbNm-Q-eSHs7shFQtZ5ehQ9OrvQjHpkNIBlsqvS2wc-Nxju5HQ3Of01pYoZnUozkr5T10S-HVtq1QiXB2EREa9cdQJBKFzp2v9zhc9TXqPsyvYHYx';
